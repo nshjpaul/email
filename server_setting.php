340ef88750c9be6ab6691497ca452662cc287135
JEAN PAUL NSHIMIYIMANA <nshjpaul@gmail.com>
2021-03-08 18:25:30 +0200

    change credencials

--- server_setting.php
+++ server_setting.php
@@ -2,8 +2,8 @@
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.googlemail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'gecoafricaltd1@gmail.com';                 // SMTP username
-        $mail->Password = 'geco/001';                           // SMTP password
+        $mail->Username = 'eazhubtest@gmail.com';                 // SMTP username
+        $mail->Password = 'eazhubtest2020!';                           // SMTP password
         $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 465;                                    // TCP port to connect to                              // TCP port to connect to
 ?>
